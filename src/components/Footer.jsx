716a968bc73df5a14f7f784769f0589a7d1aa373
RishabVer <139073388+RishabVer@users.noreply.github.com>
2024-10-27 17:43:35 +0530

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">TASTE THE EXTRAORDINARY</div>
+          <div className="right">
+            <p>Sector-6 Rohini West, New Delhi</p>
+            <p>Open: 01:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By BITEDELIGHTCODING</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By BiteDelightCoding.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
